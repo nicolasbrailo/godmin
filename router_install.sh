f8bf427725b7a7763b214751d04d89b3eee97924
nicolasbrailo@gmail.com <nicolasbrailo@gmail.com@a444944e-a9ba-df01-7af2-6c435a946789>
2011-09-16 21:14:21 +0000

    Added dependency check for bind

--- router_install.sh
+++ router_install.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+PREREQS="bind9" # isc-dhcp-server"
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
+
+for pak in $PREREQS; do
+	INS=$(dpkg -l "$pak" 2>&1 | grep "$pak" | egrep -v 'none|No packages found' | wc -l)
+	if (( $INS==0 )); then
+		echo "Package $pak missing. Can't continue."
+		echo "To install it type: sudo apt-get install $pak"
+		exit
+	fi
+done
+
 
 function get_user_input()
 {
@@ -25,5 +37,20 @@ get_user_input 'Type the name of the WAN interface' eth0 wan_iface
 get_user_input 'Type the name of the LAN interface' eth1 lan_iface
 get_user_input 'Type the IP of the LAN interface (GW)' '192.168.1.1' lan_ip
 
-source set_forwards_nat_routing.sh $wan_iface $lan_iface $lan_ip "/home/nico"
+# source set_forwards_nat_routing.sh $wan_iface $lan_iface $lan_ip "/home/nico"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
