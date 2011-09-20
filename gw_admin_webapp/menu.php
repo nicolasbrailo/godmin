52f9b81a2112814721061540523c259eba67c063
rtohme@gmail.com <rtohme@gmail.com@a444944e-a9ba-df01-7af2-6c435a946789>
2011-09-20 01:01:44 +0000

    CSS changes related to menu.php

--- gw_admin_webapp/menu.php
+++ gw_admin_webapp/menu.php
@@ -1,4 +1,35 @@
-<a href="leases.php">Known DHCP Leases</a> |
-<a href="subnets.php">Configure DHCP subnets</a> |
-<a href="static_hosts.php">Static hosts</a> |
-<a href="dns_logs.php">DNS Logs</a> |
+<?php 
+$currentpath = $_SERVER["PHP_SELF"];
+$file = explode('/', $currentpath);
+$currentpage = $file[count($file) - 1];
+?>
+<ul class="sandbar">
+	<li id="logo"><img src="img/godmin.png" height="33" width="100" border="0" /></li>
+<?php switch ($currentpage) {
+    case "leases.php": ?>
+	<li class="active"><a href="leases.php">Known DHCP Leases</a></li>
+	<li><a href="subnets.php">Configure DHCP subnets</a></li>
+	<li><a href="static_hosts.php">Static hosts</a></li>
+    <li><a href="dns_logs.php">DNS Logs</a></li>
+<?php break;
+    case "subnets.php": ?>
+	<li><a href="leases.php">Known DHCP Leases</a></li>
+	<li class="active"><a href="subnets.php">Configure DHCP subnets</a></li>
+	<li><a href="static_hosts.php">Static hosts</a></li>
+    <li><a href="dns_logs.php">DNS Logs</a></li>
+<?php break;
+    case "static_hosts.php": ?>
+	<li><a href="leases.php">Known DHCP Leases</a></li>
+	<li><a href="subnets.php">Configure DHCP subnets</a></li>
+	<li class="active"><a href="static_hosts.php">Static hosts</a></li>
+    <li><a href="dns_logs.php">DNS Logs</a></li>
+<?php break;
+    case "dns_logs.php": ?>
+	<li><a href="leases.php">Known DHCP Leases</a></li>
+	<li><a href="subnets.php">Configure DHCP subnets</a></li>
+	<li><a href="static_hosts.php">Static hosts</a></li>
+    <li class="active"><a href="dns_logs.php">DNS Logs</a></li>
+<?php break;
+}
+?>
+</ul>
