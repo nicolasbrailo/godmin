1616cefd352bffbdf058a4b85f22d6b512622f0c
nicolasbrailo@gmail.com <nicolasbrailo@gmail.com@a444944e-a9ba-df01-7af2-6c435a946789>
2011-09-20 15:31:58 +0000

    Added content filter page Updated and refactored menu

---
+++ gw_admin_webapp/content_filter.php
@@ -0,0 +1,10 @@
+<?
+include_once 'config.php';
+?>
+
+<link rel="stylesheet" href="style.css">
+
+<? include 'menu.php' ?>
+
+<img src="img/thought_police.jpg"/>
+
--- gw_admin_webapp/menu.php
+++ gw_admin_webapp/menu.php
@@ -1,38 +1,32 @@
-<?php 
+<?php
 $currentpath = $_SERVER["PHP_SELF"];
 $file = explode('/', $currentpath);
 $currentpage = $file[count($file) - 1];
 ?>
+
 <ul class="sandbar">
-	<li id="logo"><a href="https://code.google.com/p/godmin/">
-		<img src="img/godmin.png" height="33" width="100" border="0" />
-		</a>
-	</li>
-<?php switch ($currentpage) {
-    case "leases.php": ?>
-	<li class="active"><a href="leases.php">Known DHCP Leases</a></li>
-	<li><a href="subnets.php">Configure DHCP subnets</a></li>
-	<li><a href="static_hosts.php">Static hosts</a></li>
-    <li><a href="dns_logs.php">DNS Logs</a></li>
-<?php break;
-    case "subnets.php": ?>
-	<li><a href="leases.php">Known DHCP Leases</a></li>
-	<li class="active"><a href="subnets.php">Configure DHCP subnets</a></li>
-	<li><a href="static_hosts.php">Static hosts</a></li>
-    <li><a href="dns_logs.php">DNS Logs</a></li>
-<?php break;
-    case "static_hosts.php": ?>
-	<li><a href="leases.php">Known DHCP Leases</a></li>
-	<li><a href="subnets.php">Configure DHCP subnets</a></li>
-	<li class="active"><a href="static_hosts.php">Static hosts</a></li>
-    <li><a href="dns_logs.php">DNS Logs</a></li>
-<?php break;
-    case "dns_logs.php": ?>
-	<li><a href="leases.php">Known DHCP Leases</a></li>
-	<li><a href="subnets.php">Configure DHCP subnets</a></li>
-	<li><a href="static_hosts.php">Static hosts</a></li>
-    <li class="active"><a href="dns_logs.php">DNS Logs</a></li>
-<?php break;
-}
-?>
+        <li id="logo"><a href="https://code.google.com/p/godmin/">
+                <img src="img/godmin.png" height="33" width="100" border="0" />
+                </a>
+        </li>
+
+        <li <? if ($currentpage=="leases.php"){ ?> class="active"<? } ?>>
+                <a href="leases.php">Known DHCP Leases</a>
+        </li>
+
+        <li <? if ($currentpage=="subnets.php"){ ?> class="active"<? } ?>>
+                <a href="subnets.php">Configure DHCP subnets</a>
+        </li>
+
+        <li <? if ($currentpage=="static_hosts.php"){ ?> class="active"<? } ?>>
+                <a href="static_hosts.php">Static hosts</a>
+        </li>
+
+        <li <? if ($currentpage=="dns_logs.php"){ ?> class="active"<? } ?>>
+                <a href="dns_logs.php">DNS Logs</a>
+        </li>
+
+        <li <? if ($currentpage=="content_filter.php"){ ?> class="active"<? } ?>>
+                <a href="content_filter.php">Content Filter</a>
+        </li>
 </ul>
