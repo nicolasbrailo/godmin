b38fd5322aa257f7581b171350de43c28439267b
nicolasbrailo@gmail.com <nicolasbrailo@gmail.com@a444944e-a9ba-df01-7af2-6c435a946789>
2011-09-28 19:58:36 +0000

    Added restart service buttons to system status tab

--- godmin_webapp/system_status.php
+++ godmin_webapp/system_status.php
@@ -1,10 +1,44 @@
 <?
 include_once 'config.php';
+
 include 'layout/header.php';
+
+if (isset($_REQUEST['restart']))
+{
+	?>
+	<div class="alert_box">You have requested to restart <?= $_REQUEST['restart'] ?> services.
+	 Beware that this may affect active connections.<br/>Are you sure you want to proceed?
+	<form method="post" action="system_status.php">
+		<input type="submit" value="Confirm"/>
+		<input type="hidden" name="confirm_restart" value="<?= $_REQUEST['restart'] ?>"/>
+	</form>
+	</div>
+	<?
+} else if (isset($_POST['confirm_restart'])) {
+	switch($_POST['confirm_restart']) {
+		case 'DHCP':
+			OS::restart_dhcp();
+			break;
+		case 'DNS':
+			OS::restart_dns();
+			break;
+		case 'Routing':
+			OS::restart_nat_and_fwds();
+			break;
+	}
+}
 ?>
 
 <h1>System status</h1>
+<table style="width: 500px"><tr><td>
 <? include 'helpers/print_services_status.php' ?>
+</td><td>
+<ul>
+	<li><a href="system_status.php?restart=DHCP">Restart DHCP</a></li>
+	<li><a href="system_status.php?restart=DNS">Restart DNS</a></li>
+	<li><a href="system_status.php?restart=Routing">Restart routing, NAT and forwards</a></li>
+</ul>
+</td></tr></table>
 
 <br/><br/><br/>
 
